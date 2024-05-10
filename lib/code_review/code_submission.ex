429418958f2113c803284a69ae27351ed0a4c977
Juanzeen <jziignerdev@gmail.com>
2024-05-10 19:56:49 -0300

    putting in the correct arch

--- lib/code_review/code_submission.ex
+++ lib/code_review/code_submission.ex
@@ -3,5 +3,53 @@ defmodule CodeReview.CodeSubmission do
   Módulo responsável pela submissão e manipulação de snippets de código.
   """
 
+def create_id(length) do
+  length
+  |> random_bytes()
+  |> Base.url_encode64(padding: false)
+  |> take_first_chars(length)
+end
+
+defp random_bytes(n) do
+  :crypto.strong_rand_bytes(n)
+end
+
+defp take_first_chars(string, n) do
+  String.slice(string, 0, n)
+end
+
+def creating_table() do
+  codes_table = :ets.new(:submitted_codes, [:set, :protected, :named_table])
+end
+
+def submit_code(submission_map) do
+  with true <- submission_map[:snip] |> is_binary(),
+       true <- submission_map[:language] |> is_binary(),
+       true <- submission_map[:description] |> is_binary() do
+    user_id = create_id(3)
+    map_with_id = Map.put(submission_map, :author_id, user_id)
+    submission_id = create_id(4)
+    :ets.insert_new(:submitted_codes, {submission_id, map_with_id})
+    :ok
+  end
+end
+
+def get_submissions() do
+  :ets.tab2list(:submitted_codes)
+end
+
+def get_submission(id) do
+  :ets.lookup(:submitted_codes, id)
+  |> get_submission_map
+end
+
+def get_submission_map(submitted_code_list) do
+  [hd | tl] = submitted_code_list
+  {id, sub_map} = hd
+  sub_map
+end
+
+
+
   @behaviour CodeReview.CodeSubmissionBehaviour
 end
--- lib/code_review/code_submission_behaviour.ex
+++ lib/code_review/code_submission_behaviour.ex
@@ -12,30 +12,11 @@ defmodule CodeReview.CodeSubmissionBehaviour do
   Funções para gerar o ID, utilizando base64
   """
 
-  def create_id(length) do
-    length
-    |> random_bytes()
-    |> Base.url_encode64(padding: false)
-    |> take_first_chars(length)
-  end
-
-  defp random_bytes(n) do
-    :crypto.strong_rand_bytes(n)
-  end
-
-  defp take_first_chars(string, n) do
-    String.slice(string, 0, n)
-  end
-
   @doc """
   Utilizamos o ETS para adicionar os mapas em uma lista. Para isso, criamos uma tabela ets com o nome :submitted_codes,
   com as opções set -> para colocar  uma chave para cada valor, sem chaves duplicadas, caso seja colocada a mesma chave duas vezes.
   protected, qualquer um pode ver mas somente pessoas autorizadas podem lidar com a tabela e o :name_table para que possamos invocar a tabela pelo nome dado a ela
   """
-
-  def creating_table() do
-    codes_table = :ets.new(:submitted_codes, [:set, :protected, :named_table])
-  end
 
   @doc """
   Submete um novo snippet de código ao sistema. Antes de submeter é checado se o usuário manda todos os dados como strings, caso sim, geramos um ID para o autor da submissão
@@ -44,17 +25,6 @@ defmodule CodeReview.CodeSubmissionBehaviour do
   """
 
   @callback submit_code(submission_t) :: {:ok, Map.t()} | {:error, String.t()}
-  def submit_code(submission_map) do
-    with true <- submission_map[:snip] |> is_binary(),
-         true <- submission_map[:language] |> is_binary(),
-         true <- submission_map[:description] |> is_binary() do
-      user_id = create_id(3)
-      map_with_id = Map.put(submission_map, :author_id, user_id)
-      submission_id = create_id(4)
-      :ets.insert_new(:submitted_codes, {submission_id, map_with_id})
-      :ok
-    end
-  end
 
   @doc """
   Atualiza um snippet de código existente.
@@ -67,18 +37,5 @@ defmodule CodeReview.CodeSubmissionBehaviour do
   . A segunda função resgata uma única submissão com base no ID, que ao fim retorna somente o mapa
   """
   @callback get_submissions :: list(submission_t)
-  def get_submissions() do
-    :ets.tab2list(:submitted_codes)
-  end
 
-  def get_submission(id) do
-    :ets.lookup(:submitted_codes, id)
-    |> get_submission_map
-  end
-
-  def get_submission_map(submitted_code_list) do
-    [hd | tl] = submitted_code_list
-    {id, sub_map} = hd
-    sub_map
-  end
 end
